da9f462136c8e4a1aa0af1244a5cbc9d1962a1ea
Леонид Кухарчик <kuxarchik007@mail.ru>
2024-05-25 12:30:09 +0300

    Создали вендинговую машину, которая содержит слоты

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -1,1 +1,32 @@
 #include "VendingMachine.h"
+
+VendingMachine::VendingMachine(int slotCount)
+{
+	slots.resize(slotCount, nullptr);
+}
+
+bool VendingMachine::addSlot(SnackSlot* slot)
+{
+	for (size_t i = 0; i < slots.size(); ++i)
+	{
+		if (slots[i] == nullptr)
+		{
+			slots[i] = slot;
+			return true;
+		}
+	}
+	return false;
+}
+
+int VendingMachine::getEmptySlotsCount() const
+{
+	int emptyCount = 0;
+	for (const auto& slot : slots)
+	{
+		if (slot == nullptr)
+		{
+			++emptyCount;
+		}
+	}
+	return emptyCount;
+}
--- VendingMachine.h
+++ VendingMachine.h
@@ -1,5 +1,14 @@
 #pragma once
+#include<vector>
+#include "SnackSlot.h"
+
 class VendingMachine
 {
+private:
+	std::vector<SnackSlot*> slots;
 
+public:
+	VendingMachine(int slotCount);
+	bool addSlot(SnackSlot* slot);
+	int getEmptySlotsCount() const;
 };
